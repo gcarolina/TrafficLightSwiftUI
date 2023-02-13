452b77aa35c012f644e5e8d2da7956c9aeb03fd0
gcarolina <karolinaganizoda@gmail.com>
2023-02-13 22:33:18 +0300

    Project created

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Carolina on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
